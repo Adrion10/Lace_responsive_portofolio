d328e40dbd0cddd8f3419f77e7b59383c2fcccc5
Adrion10 <lacehila@gmail.com>
2022-07-02 10:40:35 +0200

    create route

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Courses from "./pages/Courses";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <div className="App">
-      <h1>Portofolio</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="project" element={<Projects />} />
+          <Route path="courses" element={<Courses />} />
+          <Route path="contact" element={<Contact />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
