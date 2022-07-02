c52418b9f238334448f1ba3184a264d554ab984e
Adrion10 <lacehila@gmail.com>
2022-07-02 21:35:58 +0200

    header styling

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { RiMenu3Fill, RiCloseFill } from "react-icons/ri";
+import { Link } from "react-router-dom";
 const Header = () => {
   const [showHeader, setShowHeader] = useState(false);
   return (
@@ -19,6 +20,23 @@ const Header = () => {
           }}
         />
       )}
+      <ul className={`${showHeader ? "show-header" : "hide-header "} n-box1`}>
+        <li>
+          <Link to="/">Home</Link>{" "}
+        </li>
+
+        <li>
+          <Link to="/project">Project</Link>
+        </li>
+
+        <li>
+          <Link to="courses">Courses</Link>
+        </li>
+
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
     </div>
   );
 };
