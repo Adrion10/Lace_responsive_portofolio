984e90d1b34491b73cb2d702930e72e305bbc2b7
Adrion10 <lacehila@gmail.com>
2022-07-10 19:20:49 +0200

    contact

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Header from "../components/Header";
 
 const Contact = () => {
   return (
     <div>
+      <Header />
       <div className="container contact mt-5">
         <div className="row pt-5">
           <div className="col-md-6 p-5">
@@ -10,11 +12,13 @@ const Contact = () => {
           </div>
           <div className="col-md-6">
             <div className="contact-form m-2 p-5 n-box2">
-              <input type="text" className="form-controll" placeholder="Name" />
+              <h3 className="font-bold">Contact US</h3>
 
-              <input type="text" className="form-controll" placeholder="Name" />
-              <textarea className="form-controll" rows={3} defaultValue={""} />
-              <button className="primary-button">SUBMIT</button>
+              <hr />
+              <input type="text" className="form-control" placeholder="Name" />
+              <input type="text" className="form-control" placeholder="Name" />
+              <textarea className="form-control" rows={3} defaultValue={""} />
+              <button className="primary-button mt-3">SUMBIT</button>
             </div>
           </div>
         </div>
